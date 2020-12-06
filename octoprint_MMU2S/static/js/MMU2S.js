611ea8eb4a9c4e5c4993a25e20fa90dd1190fa0c
Andrew Paterson <andrewjp97@me.com>
2020-12-06 19:54:05 +0000

    First Commit

---
+++ octoprint_MMU2S/static/js/MMU2S.js
@@ -0,0 +1,29 @@
+/*
+ * View model for MMU2S
+ *
+ * Author: Andrew Paterson
+ * License: AGPLv3
+ */
+$(function() {
+    function Mmu2sViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: Mmu2sViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_MMU2S, #tab_plugin_MMU2S, ...
+        elements: [ /* ... */ ]
+    });
+});
